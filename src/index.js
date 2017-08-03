7af5293845d2856e163bae5f0389473feff91709
jaketrent <trent.jake@gmail.com>
2017-08-03 14:00:22 -0600

    styled components with web elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
-const App = _ => <div>Wow</div>
+const Div = styled.div`
+  display: inline-block;
+  margin: 7px 1px;
+  padding: 2px;
+  color: ${props => props.color};
+`
+
+const CoolerDiv = Div.extend`
+  background: purple;
+`
+
+const App = _ =>
+  <div>
+    <Div color="purple">Styled</Div>
+    <CoolerDiv color="white">Components</CoolerDiv>
+  </div>
 
 ReactDOM.render(<App />, document.getElementById('app'))
