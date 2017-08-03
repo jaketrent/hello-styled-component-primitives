ac89d132a4f04809eac1b4f1db93fa7abefb3017
jaketrent <trent.jake@gmail.com>
2017-08-03 14:38:45 -0600

    add button with click handler

--- src/index.js
+++ src/index.js
@@ -12,22 +12,34 @@ const Txt = primitives.Text`
   border-left-width: 1
 `
 
-const Div = styled.div`
+const Button = primitives.Touchable({})
+
+const ButtonTxt = primitives.Text`
+  color: orange;
+  border-color: orange;
+  border-style: solid;
+  border-width: 1;
+  padding: 8px;
+  border-radius: 4px;
+`
+
+const WebDiv = styled.div`
   display: inline-block;
   margin: 7px 1px;
   padding: 2px;
   color: ${props => props.color};
 `
 
-const CoolerDiv = Div.extend`
+const CoolerWebDiv = WebDiv.extend`
   background: purple;
 `
 
 const App = _ =>
   <div>
-    <Div color="purple">Styled</Div>
-    <CoolerDiv color="white">Components</CoolerDiv>
+    <WebDiv color="purple">Styled</WebDiv>
+    <CoolerWebDiv color="white">Components</CoolerWebDiv>
     <Txt>Primitives</Txt>
+    <Button onPressIn={console.log}><ButtonTxt>Boop!</ButtonTxt></Button>
   </div>
 
 ReactDOM.render(<App />, document.getElementById('app'))
